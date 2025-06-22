adc5da5e0f584993310beda2c17425f95a853a2d
Sarja <sajabadjie8@gmail.com>
2025-06-22 13:39:56 -0400

    Refactor SQL table schema with SqlUtil constant

--- src/main/java/storage/db/SQLiteFileMetadataRepository.java
+++ src/main/java/storage/db/SQLiteFileMetadataRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.function.Function;
 
 import storage.FileObject;
+import util.SqlUtil;
 
 /**
  * SQLiteFileMetadataRepository is a concrete implementation of the
@@ -41,20 +42,8 @@ public class SQLiteFileMetadataRepository implements FileMetadataRepository {
      * @return 
      */
     private void createTableIfNotExists() {
-        String sql = """
-                        CREATE TABLE IF NOT EXISTS file_metadata (
-                            file_name TEXT PRIMARY KEY,
-                            last_modified_date INTEGER NOT NULL,
-                            checksum TEXT NOT NULL,
-                            file_path TEXT,
-                            file_size INTEGER,
-                            version INTEGER DEFAULT 1,
-                            bucket TEXT NOT NULL
-                        );
-               """;
-
         try (Statement stmt = connection.createStatement()) {
-            stmt.execute(sql);
+            stmt.execute(SqlUtil.FILE_METADATA_SQL_SCHEMA);
         } catch (SQLException e) {
             throw new RuntimeException("Failed to create table", e);
         }
